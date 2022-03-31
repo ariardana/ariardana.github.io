02515052d7d5eb08cadd5a54d51be1232b9a2efb
ariardana <102762708+ariardana@users.noreply.github.com>
2022-04-01 02:45:45 +0700

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,49 @@
+const projek = [{
+        nama: 'Tembak MacOs',
+        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/somebody',
+        web: 'https://ariiardana.github.io/somebody',
+        desc: 'Buat Nembak Ceue :v',
+        dibuat: 'HTML,CSS, Javascript',
+        status: 'selesai',
+        foto: './assets//img/img1.png'
+    },
+    {
+        nama: 'Info Covid-19',
+        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/covid',
+        web: 'https://ariardana.my.id/covid',
+        desc: 'Info Covid-19 Real-Time',
+        dibuat: 'HTML, CSS, Javascript',
+        status: 'selesai',
+        foto: './assets//img/img2.png'
+    },
+                {
+                        
+        nama: 'Selamat Hari Raya Idul Fitri 1441 H.',
+        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/idulfitri',
+        web: 'https://ariardana.my.id/idulfitri',
+        desc: '',
+        dibuat: 'HTML, CSS, Javascript',
+        status: 'selesai',
+        foto: './assets//img/img3.png'
+                },
+                {
+                        
+        nama: 'Flappy Bird Game',
+        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/flappy-bird',
+        web: 'https://ariardana.my.id/flappy-bird',
+        desc: '',
+        dibuat: 'HTML, VanillaJS',
+        status: 'selesai',
+        foto: './assets//img/img4.png'
+                },
+                {
+                        
+        nama: 'Web Kelas XII TKJ 2',
+        sc: '',
+        web: 'https://xiitkj2.my.id/',
+        desc: '',
+        dibuat: 'HTML, Bootstrap, CSS, Javascript',
+        status: 'selesai',
+        foto: './assets//img/img5.png'
+                },
+]
