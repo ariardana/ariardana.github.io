a5ae9eeb84dac261ca6d06237ef983bd4464fd4e
ariardana <102762708+ariardana@users.noreply.github.com>
2022-06-12 22:20:16 +0700

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,49 +1,50 @@
 const projek = [{
         nama: 'Tembak MacOs',
         sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/somebody',
-        web: 'https://ariiardana.github.io/somebody',
+        web: 'https://ariardana.github.io.github.io/somebody',
         desc: 'Buat Nembak Ceue :v',
         dibuat: 'HTML,CSS, Javascript',
         status: 'selesai',
-        foto: './assets//img/img1.png'
+        foto: './assets/img/img1.png'
     },
     {
         nama: 'Info Covid-19',
-        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/covid',
-        web: 'https://ariardana.my.id/covid',
+        sc: 'https://github.com/ariardana/ariardana.github.io/tree/master/covid',
+        web: 'https://ariardanaa.my.id/covid',
         desc: 'Info Covid-19 Real-Time',
         dibuat: 'HTML, CSS, Javascript',
         status: 'selesai',
-        foto: './assets//img/img2.png'
+        foto: './assets/img/img2.png'
     },
-                {
-                        
-        nama: 'Selamat Hari Raya Idul Fitri 1441 H.',
-        sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/idulfitri',
-        web: 'https://ariardana.my.id/idulfitri',
-        desc: '',
-        dibuat: 'HTML, CSS, Javascript',
-        status: 'selesai',
-        foto: './assets//img/img3.png'
-                },
                 {
                         
         nama: 'Flappy Bird Game',
         sc: 'https://github.com/ariiardana/ariiardana.github.io/tree/master/flappy-bird',
-        web: 'https://ariardana.my.id/flappy-bird',
+        web: 'https://ariardanaa.my.id/flappy-bird',
         desc: '',
         dibuat: 'HTML, VanillaJS',
         status: 'selesai',
-        foto: './assets//img/img4.png'
+        foto: './assets/img/img4.png'
                 },
                 {
                         
         nama: 'Web Kelas XII TKJ 2',
         sc: '',
         web: 'https://xiitkj2.my.id/',
-        desc: '',
-        dibuat: 'HTML, Bootstrap, CSS, Javascript',
+        desc: 'Website Kelas XII TKJ 2',
+        dibuat: 'Bootstrap, Javascript',
         status: 'selesai',
-        foto: './assets//img/img5.png'
+        foto: './assets/img/img5.png'
                 },
+                {
+                        
+        nama: 'YouTube Downloader',
+        sc: '',
+        web: 'https://azusa-yt.herokuapp.com',
+        desc: 'Buat download video youtube',
+        dibuat: 'Bootstrap, NodeJS',
+        status: 'selesai',
+        foto: './assets/img/img9.png'
+                },
+
 ]
